255b169c5335b3f3abe3e228b7173c04ef446193
Thanaphat <sukchuen_t@su.ac.th>
2025-11-29 09:18:49 +0000

    fix_front

--- front-end/src/SellItemPage.jsx
+++ front-end/src/SellItemPage.jsx
@@ -47,7 +47,7 @@ const DraggableImage = ({ image, index, moveImage, removeImage }) => {
         onClick={() => removeImage(index)}
         className="absolute top-1 right-1 bg-red-500 text-white rounded-full w-6 h-6 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity"
       >
-        ×
+        
       </button>
       {/* หมายเลขลำดับ */}
       <div className="absolute bottom-1 left-1 bg-black bg-opacity-50 text-white text-xs px-2 py-1 rounded">
